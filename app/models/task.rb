194ea15f79564408e6a43bf4f548956748def329
Ivan Rodriguez <ivan.rodriguez@sonatasmx.com>
2014-12-16 14:35:26 -0800

    added validations and converted before save to before validate

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,23 @@
 class Task < ActiveRecord::Base
-  mount_uploader :attachment, AttachmentUploader
   include PgSearch
-  pg_search_scope :search_by_description, :against => [:description,:tags]
+  mount_uploader :attachment, AttachmentUploader
+
+  pg_search_scope :search_by_description,       :against => [:description,:tags]
+
   belongs_to :user
-  before_save :default_values
-  scope :default_order, order("position")
+
+  before_validation :default_values
+
+  validates :description,                                         presence: true
+  validates :status,                                              presence: true
+  validates :user,                                                presence: true
+  validates :expiration,                                          presence: true
+  validates :position,                                            presence: true
+
+  scope :default_order,                                        order("position")
+
+  private
+
   def default_values
     self.status ||= 0
   end
